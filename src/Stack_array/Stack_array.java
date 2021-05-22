ecd9f5cbeb756af492d609752ba276bf529b2027
Mateja-Djeric <bilosta12321@gmail.com>
2021-05-22 15:43:51 +0200

    Implemented stack as array

---
+++ src/Stack_array/Stack_array.java
@@ -0,0 +1,36 @@
+package Stack_array;
+public class Stack_array {
+   private int stack_size;
+   private int stack[];
+   private int last;
+
+   public Stack_array(int stack_size) {
+       this.stack_size = stack_size;
+       this.stack = new int[stack_size];
+       this.last = 0;
+   }
+   
+   public void push(int element){
+       //stavlja poslednji element
+       if(last<stack_size){
+            this.stack[last] = element;
+            last++;
+       }
+       else{
+           System.out.println("Izvini prike nemre to tako");
+       }
+   }
+   
+   public int pop(){
+       int n;
+       if(last>0){
+            n = this.stack[last-1];
+            this.stack[last-1] = 0;
+            last--;
+            return n;
+       }
+       else{
+           return 0;
+       }
+   }
+}
